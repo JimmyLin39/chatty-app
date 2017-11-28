0bdd1c66829056dd9f506e81d4049ca17e0cbc85
JimmyLin39 <jimmylin0309@email.com>
2017-11-28 12:45:44 -0800

    Pass currentUser using props

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,19 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
         {
-          username: "Bob",
-          content: "Has anyone seen my marbles?",
+          username: 'Bob',
+          content: 'Has anyone seen my marbles?',
         },
         {
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+          username: 'Anonymous',
+          content: 'No, I think you lost them. You lost your marbles Bob. You lost them for good.'
         }
       ]
     };
   }
-
-
 
   render() {
     return (
@@ -31,7 +29,7 @@ class App extends Component {
           <a href="/" className="navbar-brand">Chatty</a>
         </nav>
         <MessageList />
-        <ChatBar />
+        <ChatBar currentUser={ this.state.currentUser } />
       </div>
     )   
   }
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -4,9 +4,10 @@ console.log('Rendering <ChatBar/>');
 
 class ChatBar extends Component {
   render() {
+    const currentUser = this.props.currentUser.name;     
     return (
       <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
+        <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={ currentUser } />
         <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
       </footer>
     );
