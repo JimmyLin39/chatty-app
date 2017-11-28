8f160882354368e1b8d372b1c8d7e581e3a202a8
JimmyLin39 <jimmylin0309@email.com>
2017-11-27 19:21:05 -0800

    move messageList elements to MessageList.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
 
 console.log("Rendering <App/>");
 
 class App extends Component {
   render() {
     return (
-    <div>
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-      </nav>
-      
-    </div>
-    );
+      <div>  
+        <nav className="navbar">
+          <a href="/" className="navbar-brand">Chatty</a>
+        </nav>
+        <MessageList />
+      </div>
+    )   
   }
 }
 export default App;
