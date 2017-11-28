cf1d3945730fe2cc2c623486cb1967e73e9c5d93
JimmyLin39 <jimmylin0309@email.com>
2017-11-28 14:03:36 -0800

    setTimeout waits 3 seconds and then adds a new message

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import ChatBar from './ChatBar.jsx';
 console.log('Rendering <App/>');
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
       currentUser: {name: 'Bob'}, // optional. if currentUser is not defined, it means the user is Anonymous
       messages: [
@@ -22,6 +22,19 @@ class App extends Component {
         }
       ]
     };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
 
   render() {
