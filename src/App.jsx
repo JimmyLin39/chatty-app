56933482ff885124d3ecc294958ce8cb2540d03c
JimmyLin39 <jimmylin0309@email.com>
2017-11-28 12:21:11 -0800

    set initial state

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,25 @@ import ChatBar from './ChatBar.jsx';
 console.log('Rendering <App/>');
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    };
+  }
+
+
+
   render() {
     return (
       <div>  
